320b5778be5bc5b25627cbb60c154c2ad488427f
Ian Duggan <ian@ianduggan.net>
2026-06-06 20:10:00 -0400

    dev-ip 0.1.0: refresh sha (dnsmasq hardening + integration test)

--- Formula/dev-ip.rb
+++ Formula/dev-ip.rb
@@ -2,7 +2,7 @@ class DevIp < Formula
   desc "Static per-workspace loopback IP + hostname allocator for macOS"
   homepage "https://github.com/ijcd/dev-ip"
   url "https://github.com/ijcd/dev-ip/archive/v0.1.0.tar.gz"
-  sha256 "f503ad7ce734fd49923b89bbfd000e374d944ca406198851086046a4c88ef734"
+  sha256 "b752db4eea87e7807b15e41d1d4f503d01a79633bc8a04f75d54e180af8b3d1f"
   license "GPL-3.0-or-later"
 
   depends_on :macos
