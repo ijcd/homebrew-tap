0562ccdfdf27854544cde0a988e50c8eb0267cc0
Ian Duggan <ian@ianduggan.net>
2026-06-08 11:10:00 -0400

    dev-ip 0.1.1: pf.conf ordering + dnsmasq agent path/restart/status fixes

--- Formula/dev-ip.rb
+++ Formula/dev-ip.rb
@@ -1,8 +1,8 @@
 class DevIp < Formula
   desc "Static per-workspace loopback IP + hostname allocator for macOS"
   homepage "https://github.com/ijcd/dev-ip"
-  url "https://github.com/ijcd/dev-ip/archive/v0.1.2.tar.gz"
-  sha256 "defea0280b980288b281bd7e02d468e5a27c31af9b6274f25a4aa165dd7166fc"
+  url "https://github.com/ijcd/dev-ip/archive/v0.1.1.tar.gz"
+  sha256 "8d90d11c28135046de637a87cd8897ceef0c2b352bf23f6c4a4a90f170789ed6"
   license "GPL-3.0-or-later"
 
   depends_on :macos
